a992fefbccf36f66f9e4332fb284d5b9a2bd2c97
lazy_owl <slimsly11@gmail.com>
2022-11-24 03:38:51 +0600

    constructor comments

--- array/array.go
+++ array/array.go
@@ -5,18 +5,21 @@ type Array[T any] struct {
 	iterator int
 }
 
+// New creates new empty collection
 func New[T any]() *Array[T] {
 	return &Array[T]{
 		items: make([]T, 0),
 	}
 }
 
+// NewWithSize creates new empty collection with given capacity
 func NewWithSize[T any](size int) *Array[T] {
 	return &Array[T]{
 		items: make([]T, 0, size),
 	}
 }
 
+// NewWithList creates new collection with given slice with values
 func NewWithList[T any](items ...T) *Array[T] {
 	return &Array[T]{
 		items: items,
--- set/set.go
+++ set/set.go
@@ -5,18 +5,21 @@ type Set[T any] struct {
 	iterator int
 }
 
+// New creates new empty collection
 func New[T any]() *Set[T] {
 	return &Set[T]{
 		items: make([]T, 0),
 	}
 }
 
+// NewWithSize creates new empty collection with given capacity
 func NewWithSize[T any](size int) *Set[T] {
 	return &Set[T]{
 		items: make([]T, 0, size),
 	}
 }
 
+// NewWithList creates new collection with given slice with values
 func NewWithList[T any](items ...T) *Set[T] {
 	return &Set[T]{
 		items: items,
