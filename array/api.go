76fe378fdacd7ad68eeb18c2afd66533cc5023d8
lazy_owl <slimsly11@gmail.com>
2022-11-24 03:37:24 +0600

    methods comments

--- array/api.go
+++ array/api.go
@@ -4,21 +4,25 @@ import (
 	"sort"
 )
 
+// Push add element to the end. Add only if value is unique
 func (array *Array[T]) Push(value T) *Array[T] {
 	array.items = append(array.items, value)
 	return array
 }
 
+// PushForward add element to the start
 func (array *Array[T]) PushForward(value T) *Array[T] {
 	array.items = append([]T{value}, array.items...)
 	return array
 }
 
+// PushList add slice/array of elements to the end
 func (array *Array[T]) PushList(items ...T) *Array[T] {
 	array.items = append(array.items, items...)
 	return array
 }
 
+// Pop remove last element
 func (array *Array[T]) Pop() *Array[T] {
 	size := array.Size()
 	if size == 0 {
@@ -29,6 +33,7 @@ func (array *Array[T]) Pop() *Array[T] {
 	return array
 }
 
+// PopForward remove first element
 func (array *Array[T]) PopForward() *Array[T] {
 	size := array.Size()
 	if size == 0 {
@@ -39,6 +44,7 @@ func (array *Array[T]) PopForward() *Array[T] {
 	return array
 }
 
+// Remove remove element on index
 func (array *Array[T]) Remove(index int) *Array[T] {
 	if array.Empty() || array.indexOut(index) {
 		return array
@@ -48,16 +54,19 @@ func (array *Array[T]) Remove(index int) *Array[T] {
 	return array
 }
 
+// RemoveAll remove all elements
 func (array *Array[T]) RemoveAll() *Array[T] {
 	array.items = make([]T, 0)
 	return array
 }
 
+// Clear remove all elements with saving capacity (previous size)
 func (array *Array[T]) Clear() *Array[T] {
 	array.items = make([]T, 0, array.Size())
 	return array
 }
 
+// Get returns element on index
 func (array *Array[T]) Get(index int) T {
 	size := array.Size()
 
@@ -69,19 +78,23 @@ func (array *Array[T]) Get(index int) T {
 	return array.items[index]
 }
 
+// Empty check is empty or not
 func (array *Array[T]) Empty() bool {
 	return array.Size() == 0
 }
 
+// Size returns size of collection
 func (array *Array[T]) Size() int {
 	return len(array.items)
 }
 
+// Sort implementation of sort package
 func (array *Array[T]) Sort(compareFunc func(i, j int) bool) *Array[T] {
 	sort.Slice(array.items, compareFunc)
 	return array
 }
 
+// Next moves iterator
 func (array *Array[T]) Next() bool {
 	iterator := array.iterator
 	size := array.Size()
@@ -94,6 +107,7 @@ func (array *Array[T]) Next() bool {
 	return true
 }
 
+// Value returns value on iterator
 func (array *Array[T]) Value() T {
 	size := array.Size()
 	if size == 0 {
@@ -103,15 +117,18 @@ func (array *Array[T]) Value() T {
 	return array.items[array.iterator-1]
 }
 
+// Reset set iterator to the start
 func (array *Array[T]) Reset() *Array[T] {
 	array.iterator = 0
 	return array
 }
 
+// Clone create new collection with the same data
 func (array *Array[T]) Clone() *Array[T] {
 	return NewWithList[T](array.items...)
 }
 
+// CloneRange clone collection but with range
 func (array *Array[T]) CloneRange(from, to int) *Array[T] {
 	if from < 0 {
 		from = 0
@@ -124,14 +141,17 @@ func (array *Array[T]) CloneRange(from, to int) *Array[T] {
 	return NewWithList[T](array.SliceRange(from, to)...)
 }
 
+// IndexOf search element and returns index
 func (array *Array[T]) IndexOf(item T) int {
 	return array.find(item)
 }
 
+// Slice returns slice of collection
 func (array *Array[T]) Slice() []T {
 	return array.items
 }
 
+// SliceRange returns slice of collection with range
 func (array *Array[T]) SliceRange(from, to int) []T {
 	if from < 0 {
 		from = 0
--- set/api.go
+++ set/api.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+// Push add element to the end
 func (set *Set[T]) Push(value T) *Set[T] {
 	if helper.In[T](set.items, value) {
 		return set
@@ -14,6 +15,7 @@ func (set *Set[T]) Push(value T) *Set[T] {
 	return set
 }
 
+// PushForward add element to the start
 func (set *Set[T]) PushForward(value T) *Set[T] {
 	if helper.In[T](set.items, value) {
 		return set
@@ -23,6 +25,7 @@ func (set *Set[T]) PushForward(value T) *Set[T] {
 	return set
 }
 
+// PushList add slice/array of elements to the end
 func (set *Set[T]) PushList(items ...T) *Set[T] {
 	for _, item := range items {
 		if !helper.In[T](set.items, item) {
@@ -33,6 +36,7 @@ func (set *Set[T]) PushList(items ...T) *Set[T] {
 	return set
 }
 
+// Pop remove last element
 func (set *Set[T]) Pop() *Set[T] {
 	if set.Empty() {
 		return set
@@ -42,6 +46,7 @@ func (set *Set[T]) Pop() *Set[T] {
 	return set
 }
 
+// PopForward remove first element
 func (set *Set[T]) PopForward() *Set[T] {
 	if set.Empty() {
 		return set
@@ -51,6 +56,7 @@ func (set *Set[T]) PopForward() *Set[T] {
 	return set
 }
 
+// Remove remove element on index
 func (set *Set[T]) Remove(index int) *Set[T] {
 	if set.Empty() || set.indexOut(index) {
 		return set
@@ -60,24 +66,29 @@ func (set *Set[T]) Remove(index int) *Set[T] {
 	return set
 }
 
+// RemoveAll remove all elements
 func (set *Set[T]) RemoveAll() *Set[T] {
 	set.items = make([]T, 0)
 	return set
 }
 
+// Clear remove all elements with saving capacity (previous size)
 func (set *Set[T]) Clear() *Set[T] {
 	set.items = make([]T, 0, set.Size())
 	return set
 }
 
+// Empty check is empty or not
 func (set *Set[T]) Empty() bool {
 	return set.Size() == 0
 }
 
+// Size returns size of collection
 func (set *Set[T]) Size() int {
 	return len(set.items)
 }
 
+// Get returns element on index
 func (set *Set[T]) Get(index int) T {
 	size := set.Size()
 
@@ -89,11 +100,13 @@ func (set *Set[T]) Get(index int) T {
 	return set.items[index]
 }
 
+// Sort implementation of sort package
 func (set *Set[T]) Sort(compareFunc func(i, j int) bool) *Set[T] {
 	sort.Slice(set.items, compareFunc)
 	return set
 }
 
+// Next moves iterator
 func (set *Set[T]) Next() bool {
 	iterator := set.iterator
 	size := set.Size()
@@ -106,6 +119,7 @@ func (set *Set[T]) Next() bool {
 	return true
 }
 
+// Value returns value on iterator
 func (set *Set[T]) Value() T {
 	size := set.Size()
 	if size == 0 {
@@ -115,15 +129,18 @@ func (set *Set[T]) Value() T {
 	return set.items[set.iterator-1]
 }
 
+// Reset set iterator to the start
 func (set *Set[T]) Reset() *Set[T] {
 	set.iterator = 0
 	return set
 }
 
+// Clone create new collection with the same data
 func (set *Set[T]) Clone() *Set[T] {
 	return NewWithList[T](set.items...)
 }
 
+// CloneRange clone collection but with range
 func (set *Set[T]) CloneRange(from, to int) *Set[T] {
 	if from < 0 {
 		from = 0
@@ -136,14 +153,17 @@ func (set *Set[T]) CloneRange(from, to int) *Set[T] {
 	return NewWithList[T](set.SliceRange(from, to)...)
 }
 
+// IndexOf search element and returns index
 func (set *Set[T]) IndexOf(item T) int {
 	return set.find(item)
 }
 
+// Slice returns slice of collection
 func (set *Set[T]) Slice() []T {
 	return set.items
 }
 
+// SliceRange returns slice of collection with range
 func (set *Set[T]) SliceRange(from, to int) []T {
 	if from < 0 {
 		from = 0
