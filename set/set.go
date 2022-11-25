078301c377450119ac249a778f84def2ace67ae5
lazy_owl <slimsly11@gmail.com>
2022-11-25 23:55:02 +0600

    type list & set capacity control

--- set/set.go
+++ set/set.go
@@ -1,5 +1,12 @@
 package set
 
+import "github.com/lowl11/lazy-collection/helper"
+
+const (
+	defaultCapacity  = 100
+	increaseCapacity = 2
+)
+
 type Set[T any] struct {
 	items    []T
 	iterator int
@@ -8,20 +15,23 @@ type Set[T any] struct {
 // New creates new empty collection
 func New[T any]() *Set[T] {
 	return &Set[T]{
-		items: make([]T, 0),
+		items: make([]T, 0, defaultCapacity),
 	}
 }
 
 // NewWithSize creates new empty collection with given capacity
 func NewWithSize[T any](size int) *Set[T] {
 	return &Set[T]{
-		items: make([]T, 0, size),
+		items: make([]T, 0, size*increaseCapacity),
 	}
 }
 
 // NewWithList creates new collection with given slice with values
 func NewWithList[T any](items ...T) *Set[T] {
+	newList := make([]T, len(items), (defaultCapacity+len(items))*increaseCapacity)
+	helper.Fill[T](newList, items)
+
 	return &Set[T]{
-		items: items,
+		items: newList,
 	}
 }
--- type_list/list.go
+++ type_list/list.go
@@ -1,5 +1,12 @@
 package type_list
 
+import "github.com/lowl11/lazy-collection/helper"
+
+const (
+	defaultCapacity  = 100
+	increaseCapacity = 2
+)
+
 type TypeList[T, X any] struct {
 	items    []T
 	iterator int
@@ -8,20 +15,23 @@ type TypeList[T, X any] struct {
 // New creates new empty collection
 func New[T, X any]() *TypeList[T, X] {
 	return &TypeList[T, X]{
-		items: make([]T, 0),
+		items: make([]T, 0, defaultCapacity),
 	}
 }
 
 // NewWithSize creates new empty collection with given capacity
 func NewWithSize[T, X any](size int) *TypeList[T, X] {
 	return &TypeList[T, X]{
-		items: make([]T, 0, size),
+		items: make([]T, 0, size*increaseCapacity),
 	}
 }
 
 // NewWithList creates new collection with given slice with values
 func NewWithList[T, X any](items ...T) *TypeList[T, X] {
+	newList := make([]T, len(items), (defaultCapacity+len(items))*increaseCapacity)
+	helper.Fill[T](newList, items)
+
 	return &TypeList[T, X]{
-		items: items,
+		items: newList,
 	}
 }
